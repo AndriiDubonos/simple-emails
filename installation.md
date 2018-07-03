f29bc6a20e34b32b6d3771607c2ab9dae1f2373f
andre <andrii.dubonos@computools.com>
2018-07-03 19:53:29 +0300

    changes to structure

---
+++ installation.md
@@ -0,0 +1,8 @@
+# Installation
+
+pip install -e /path/to/simple-emails/
+
+
+# Usage
+
+from simple_emails import get_email_class
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='simple-emails',
+    version='0.1',
+    packages=['simple_emails'],
+    package_dir={'':'src'},
+    url='',
+    license='Apache License 2.0',
+    author='Denis, Andrii',
+    author_email='andrii.dubonos@computools.com',
+    description=''
+)
